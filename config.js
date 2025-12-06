09168358140372025a244c93c534c337edd826e3
huang-pei-shan <11244007@gms.tcu.edu.tw>
2025-12-06 16:46:59 +0800

    Uncomment Supabase configuration values

--- config.js
+++ config.js
@@ -6,6 +6,7 @@
 
 // config.js
 // ⚠ 記得改成你自己的 Supabase 專案 URL 與 anon 公鑰
-window.const SUPABASE_URL = "https://mfljkyvdadxlrbxlboce.supabase.co";
-window.const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs";
+const SUPABASE_URL = "https://mfljkyvdadxlrbxlboce.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbGpreXZkYWR4bHJieGxib2NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTQwMDUsImV4cCI6MjA4MDM5MDAwNX0.Z4OeacVpO8yM1d1uOWZ6jU2Gl7wgEbhXvAFSqF5pBRs";
+
 
